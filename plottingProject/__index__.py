750cf03c57c6da2904464fd88de0171c0ace1637
ManmadeArc <44855389+ManmadeArc@users.noreply.github.com>
2021-11-25 18:03:55 -0800

    No se porque pero mi vscode ejecuta el archivo desde el path del folder / workspace asi que le movi

--- plottingProject/__index__.py
+++ plottingProject/__index__.py
@@ -4,13 +4,19 @@ import matplotlib.pyplot as plt
 
 def load_plot(propertyName, graphTitle):
   fig, ax = plt.subplots()
+
+  f = open('./plottingProject/json/Modelo-90acc.json')
+  data = json.load(f)
+  ax.plot(data['epochs'][::200], data[propertyName][::200], label=f'Modelo-1')
   for i in range(3, 10):
-    f = open(f'./json/Modelo{i}.json')
+    f = open(f'./plottingProject/json/Modelo{i}.json')
 
     data = json.load(f)
 
-    ax.plot(data['epochs'], data[propertyName], label=f'Modelo-{i}')
+    ax.plot(data['epochs'], data[propertyName], label=f'Modelo-{i-2}')
 
+
+ 
   ax.legend(loc=2, bbox_to_anchor=(0.80, 1.0))
   plt.title(graphTitle)
 
